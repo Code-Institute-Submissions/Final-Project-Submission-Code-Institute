a354583ed2cd0ea3877d807692426a3d9c485ea9
Conor Guider <con.guider@gmail.com>
2019-04-19 14:28:56 +0000

    added features app

---
+++ features/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from .models import Feature
+
+# Create your tests here.
+class FeatureTest(TestCase):
+    """
+    Here we'll define the tests
+    that we'll run against our Feature models
+    """
+    
+    def test_str(self):
+        test_name = Feature(name='A feature')
+        self. assertEqual(str(test_name), 'A feature')
---
+++ features/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from .views import all_features
+
+
+urlpatterns = [
+    url(r'^$', all_features, name='features'),
+    ]
