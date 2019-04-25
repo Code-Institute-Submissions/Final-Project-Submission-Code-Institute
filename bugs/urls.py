f6c2a7bb20accb93be5270584a1b3d6fe3878ce8
Conor Guider <con.guider@gmail.com>
2019-04-25 14:20:37 +0000

    created and wired bugs_details

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_bugs, add_bug
+from .views import all_bugs, add_bug, bug_details
 
 
 urlpatterns = [
     url(r'^$', all_bugs, name='bugs'),
     url(r'^new/$', add_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/$', bug_details, name='bug_details'),
     ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from .models import Bug
 from django.contrib.auth.decorators import login_required
 from .forms import BugForm
@@ -23,3 +23,22 @@ def add_bug(request):
     else:
         form = BugForm()
     return render(request, 'add_bug.html', {'form':form})
+    
+
+@login_required() 
+def bug_details(request, pk):
+    bug = get_object_or_404(Bug, pk=pk)
+    if request.method == "POST":
+        form = BugForm(request.POST)
+        if form.is_valid():
+            bugDescrip = form.save(commit=False)
+            bugDescrip.bug = bug
+            bugDescrip.author = request.user
+            bugDescrip.save()
+            return redirect(reverse('bug_details', kwargs={'pk': pk}))
+             
+    else:
+        form = BugForm()
+        bug.views += 1
+        bug.save()
+    return render(request, 'bug_details.html', {'bug':bug, 'form':form})
